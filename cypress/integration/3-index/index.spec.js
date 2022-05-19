fe6b49526a7cc20729e669e13571a991a4d62d51
Rauan Ishida Sanfelice <rauan.sanfelice@gmail.com>
2022-05-19 15:06:14 -0300

    🧪 cypress

---
+++ cypress/integration/3-index/index.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('example validation form', () => {
+
+    const newName = 'Rick Sanchez';
+    beforeEach(() => {
+        cy.visit('/');
+    })
+
+    it('form submit ok', () => {
+        cy.get('input[name="name"]').type(newName);
+        cy.get('button[name="btn-submit"]').click();
+
+        cy.on('window:alert',(t)=>{
+            expect(t).to.contains('Ok!');
+        });
+        cy.screenshot();
+    });
+
+    it('clean form', () => {
+        cy.get('input[name="name"]').type(newName);
+        cy.get('button[name="btn-clean"]').click();
+        cy.get('input[name="name"]').should('have.text', '');
+        cy.screenshot();
+    });
+
+    it('form submit error', () => {
+        cy.get('button[name="btn-submit"]').click();
+        cy.get('span[id="error-name"]').should('contain','Campo obrigatório');
+        cy.screenshot();
+    });
+
+})
